14c458d70bb494d80f9035f4ed2d2c5c44c6329f
Chris Ham <greenham@users.noreply.github.com>
2018-04-27 07:19:26 -0700

    catch errors from task during cron

--- cron.js
+++ cron.js
@@ -14,6 +14,8 @@ db.connect(config.db.host, config.db.db, (err) => {
 });
 
 let scheduleJobs = () => {
-	const refreshSpeedgamingEventsJob = schedule.scheduleJob('*/15 * * * *', () => {tasks.refreshSpeedgamingEvents('alttp')});
+	const refreshSpeedgamingEventsJob = schedule.scheduleJob('*/15 * * * *', () => {
+		tasks.refreshSpeedgamingEvents('alttp').catch(console.error);
+	});
 	console.log(`Scheduled task 'refreshSpeedgamingEvents' for: ${refreshSpeedgamingEventsJob.nextInvocation()}`);
 }
