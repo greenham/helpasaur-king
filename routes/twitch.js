c41de7fb5b4cab921c6f0bbaf99bc202231adbcf
Chris Ham <greenham@users.noreply.github.com>
2018-06-21 07:53:40 -0700

    support joining/leaving twitch channels through web

--- routes/twitch.js
+++ routes/twitch.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   router = express.Router(),
   moment = require('moment-timezone'),
+  irc = require('irc'),
   db = require('../db');
 
 router.get('/', (req, res) => {
@@ -9,8 +10,75 @@ router.get('/', (req, res) => {
 
 // Twitch Settings Control
 router.get('/settings', (req, res) => {
-	//req.app.locals.config.twitch
+	req.app.locals.config.twitch.channels.sort();
 	res.render('twitch/settings', req.app.locals.config.twitch);
+});
+
+router.post('/channels', (req, res) => {
+	console.log(`received request to join ${req.body.channel}`);
+
+	let config = req.app.locals.config;
+
+	// Connect to Twitch IRC server and join our own channel
+  let client = new irc.Client(config.twitch.ircServer, config.twitch.username, {
+    password: config.twitch.oauth,
+    autoConnect: false,
+    channels: ['#'+config.twitch.username]
+  });
+
+  client.connect(10, () => {
+  	let message = `${config.twitch.cmdPrefix}join ${req.body.channel}`;
+  	client.say('#'+config.twitch.username, message);
+  });
+
+  client.addListener('error', message => {
+    if (message.command != 'err_unknowncommand') {
+      console.error('error from Twitch IRC Server: ', message);
+    }
+  });
+
+  client.addListener('message', (from, to, msg) => {
+  	console.log(from, to, msg);
+  	if (from === config.twitch.username && to === '#'+config.twitch.username && msg.includes(`Joining ${req.body.channel}`)) {
+  		client.disconnect();
+  	}
+  });
+
+  res.send({result: true});
+});
+
+router.delete('/channels', (req, res) => {
+	// have the bot leave requested channel
+	console.log(`received request to leave ${req.body.channel}`);
+
+	let config = req.app.locals.config;
+
+	// Connect to Twitch IRC server and join our own channel
+  let client = new irc.Client(config.twitch.ircServer, config.twitch.username, {
+    password: config.twitch.oauth,
+    autoConnect: false,
+    channels: ['#'+config.twitch.username]
+  });
+
+  client.connect(10, () => {
+  	let message = `${config.twitch.cmdPrefix}leave ${req.body.channel.substr(1)}`;
+  	client.say('#'+config.twitch.username, message);
+  });
+
+  client.addListener('error', message => {
+    if (message.command != 'err_unknowncommand') {
+      console.error('error from Twitch IRC Server: ', message);
+    }
+  });
+
+  client.addListener('message', (from, to, msg) => {
+  	console.log(from, to, msg);
+  	if (from === config.twitch.username && to === '#'+config.twitch.username && msg.includes(`Leaving ${req.body.channel}`)) {
+  		client.disconnect();
+  	}
+  });
+
+	res.send({result: true});
 });
 
 module.exports = router;
