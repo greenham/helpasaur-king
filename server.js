33de4fcf2c9bd9d5e16017a8bd43c0147fe78463
Chris Ham <github@greenhamsolutions.com>
2019-05-20 11:41:28 -0700

    attempt to fix session issue

--- server.js
+++ server.js
@@ -1,59 +1,65 @@
-var express = require('express'),
-  exphbs = require('express-handlebars'),
-  handlebars = require('./helpers/handlebars.js')(exphbs),
-  db = require('./db'),
-  logger = require('morgan'),
-  cookieParser = require('cookie-parser'),
-  bodyParser = require('body-parser'),
-  expressSanitizer = require('express-sanitizer'),
-  session = require('express-session'),
-  passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  MongoStore = require('connect-mongo')(session);
+var express = require("express"),
+  exphbs = require("express-handlebars"),
+  handlebars = require("./helpers/handlebars.js")(exphbs),
+  db = require("./db"),
+  logger = require("morgan"),
+  cookieParser = require("cookie-parser"),
+  bodyParser = require("body-parser"),
+  expressSanitizer = require("express-sanitizer"),
+  session = require("express-session"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  MongoStore = require("connect-mongo")(session);
 
 // Read local config
-let config = require('./config.json');
+let config = require("./config.json");
 
 const app = express(),
   port = process.env.PORT || config.webapp.port || 3000,
-  env = process.env.NODE_ENV || config.webapp.env || 'development';
+  env = process.env.NODE_ENV || config.webapp.env || "development";
 
 // Set app environment
-app.set('env', env);
+app.set("env", env);
 
 // Discourage exploits
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
 // Make app config available everywhere
 app.locals.config = config;
 
 // Use Handlebars for templating
-app.engine('.hbs', handlebars.engine);
-app.set('view engine', '.hbs');
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
 
 // Set up logging
 //app.use(logger('dev'));
 
 // Request parsing + sanitizing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(expressSanitizer());
 
+const getExpirationDate = sessionExpirationSeconds => {
+  return new Date(Date.now() + sessionExpirationSeconds * 1000);
+};
+
 // Set up sessions if they're configured
 if (config.webapp.session && config.webapp.session.secret) {
-  let sessionExpirationSeconds = config.webapp.session.ttl || (14 * 24 * 60 * 60);  // = 14 days. Default
-  app.use(session({
-    secret: config.webapp.session.secret,
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({
-      url: `${config.db.host}/${config.db.db}`,
-      ttl: sessionExpirationSeconds,
-      stringify: false
-    }),
-    cookie: {expires: new Date(Date.now() + sessionExpirationSeconds*1000)}
-  }));
+  let sessionExpirationSeconds = config.webapp.session.ttl || 14 * 24 * 60 * 60; // = 14 days. Default
+  app.use(
+    session({
+      secret: config.webapp.session.secret,
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({
+        url: `${config.db.host}/${config.db.db}`,
+        ttl: sessionExpirationSeconds,
+        stringify: false
+      }),
+      cookie: { expires: getExpirationDate(sessionExpirationSeconds) }
+    })
+  );
 }
 
 app.use(passport.initialize());
@@ -67,32 +73,34 @@ app.use((req, res, next) => {
 
 // Refresh userConfig on each request
 app.use((req, res, next) => {
-  db.get().collection("config").findOne({"default": true}, (err, userConfig) => {
-    if (!err) {
-      config = Object.assign(config, userConfig);
-      res.locals.config = config;
-      next();
-    } else {
-      console.error(`Unable to read config from database: ${err}`);
-    }
-  });
+  db.get()
+    .collection("config")
+    .findOne({ default: true }, (err, userConfig) => {
+      if (!err) {
+        config = Object.assign(config, userConfig);
+        res.locals.config = config;
+        next();
+      } else {
+        console.error(`Unable to read config from database: ${err}`);
+      }
+    });
 });
 
 // Routing for static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Routing for everything else
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // User auth
-var Account = require('./models/account');
+var Account = require("./models/account");
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -101,13 +109,13 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     // @TODO: Detect if this request was via ajax, in which case, send the result directly, don't render
-    res.render('error', {
-        message: err.message,
-        error: err
+    res.render("error", {
+      message: err.message,
+      error: err
     });
   });
 }
@@ -117,7 +125,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   // @TODO: Detect if this request was via ajax, in which case, send the result directly, don't render
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
@@ -125,15 +133,19 @@ app.use(function(err, req, res, next) {
 
 // Connect to Mongo and start listening on the configured port
 if (!config.db || !config.db.host || !config.db.db) {
-	console.error("Database has not been properly configured -- check config.json!");
-	process.exit(1);
+  console.error(
+    "Database has not been properly configured -- check config.json!"
+  );
+  process.exit(1);
 }
 
-db.connect(config.db.host, config.db.db, (err) => {
-	if (!err) {
-    app.listen(port, () => {console.log(`Listening on port ${port}`);});
-	} else {
-		console.error('Unable to connect to database. Check config.json!', err);
-		process.exit(1);
-	}
+db.connect(config.db.host, config.db.db, err => {
+  if (!err) {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  } else {
+    console.error("Unable to connect to database. Check config.json!", err);
+    process.exit(1);
+  }
 });
