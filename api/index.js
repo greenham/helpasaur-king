b13e85d84104f8ccfab1a778130465df8c1c816a
greenham <greenham83@gmail.com>
2022-09-05 15:45:07 -0700

    dockerize the api

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
-const mongoString = process.env.DATABASE_URL;
+const { MONGODB_URL } = process.env;
 
-mongoose.connect(mongoString);
+mongoose.connect(MONGODB_URL);
 const database = mongoose.connection;
 
 database.on("error", (error) => {
