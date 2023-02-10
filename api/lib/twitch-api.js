b43ff0c7f1d1df9db71c229fec1df5b02fa99a1a
greenham <greenham83@gmail.com>
2023-02-10 08:23:12 -0800

    lol just use the class you're extending, idiot

--- api/lib/twitch-api.js
+++ api/lib/twitch-api.js
@@ -1,57 +1,22 @@
-const axios = require("axios");
-const EventEmitter = require("events");
 const TwitchApi = require("node-twitch").default;
 const { TWITCH_EVENTSUB_SECRET_KEY, TWITCH_EVENTSUB_WEBHOOK_URL } = process.env;
 
 class TwitchApiWithEventSub extends TwitchApi {
   constructor(options) {
-    super({
-      client_id: options.clientId,
-      client_secret: options.clientSecret,
-    });
-
-    this.clientId = options.clientId;
-    this.clientSecret = options.clientSecret;
-    this.token = null;
-
-    this.eventSubApi = axios.create({
-      baseURL: "https://api.twitch.tv/helix",
-      headers: {
-        "Content-Type": "application/json",
-        "Client-ID": this.clientId,
-      },
-    });
-
-    this.emitter = new EventEmitter();
-    this.emit = this.emitter.emit;
-    this.on = this.emitter.on;
-
-    this._getTokenForEventSub();
+    super(options);
   }
 
-  _getTokenForEventSub() {
-    this._getAppAccessToken()
-      .then((token) => {
-        console.log(
-          `Twitch app access token for client ${this.clientId}: ${token}`
-        );
+  getSubscriptions(params) {
+    let queryParts = [];
+    for (const [key, value] of Object.entries(params)) {
+      queryParts.push(`${key}=${value}`);
+    }
 
-        this.token = token;
-        this.eventSubApi.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${this.token}`;
-
-        this.emit("ready");
-      })
-      .catch(console.error);
-  }
-
-  getSubscriptions(after = "") {
-    return this.eventSubApi.get(`/eventsub/subscriptions?after=${after}`);
+    return this._get(`/eventsub/subscriptions?${queryParts.join("&")}`);
   }
 
   createSubscription(userId, type, version = "1") {
-    return this.eventSubApi.post("/eventsub/subscriptions", {
+    return this._post("/eventsub/subscriptions", {
       type,
       version,
       condition: {
@@ -65,19 +30,13 @@ class TwitchApiWithEventSub extends TwitchApi {
     });
   }
 
-  deleteSubscription(params) {
-    let queryParts = [];
-    for (const [key, value] of Object.entries(params)) {
-      queryParts.push(`${key}=${value}`);
-    }
-    return this.eventSubApi.delete(
-      `/eventsub/subscriptions?${queryParts.join("&")}`
-    );
+  deleteSubscription(id) {
+    return this._delete(`/eventsub/subscriptions?id=${id}`);
   }
 
-  clearSubscriptions(after = "") {
+  clearSubscriptions(after) {
     return new Promise((resolve, reject) => {
-      this.getSubscriptions(after)
+      this.getSubscriptions({ after })
         .then((res) => {
           let subscriptionIds = res.data.data.map((d) => d.id);
           subscriptionIds.forEach((sid) => {
