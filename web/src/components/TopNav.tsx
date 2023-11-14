970e6db292127cdbe79014648c4bca13b162e75c
Chris Ham <431647+greenham@users.noreply.github.com>
2023-11-13 21:21:35 -0800

    more links

--- web/src/components/TopNav.tsx
+++ web/src/components/TopNav.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 
 function TopNav() {
@@ -33,6 +34,57 @@ function TopNav() {
                   <i className="fa-brands fa-twitch"></i> ALttP Streams
                 </Nav.Link>
               </LinkContainer>
+              <Nav.Link
+                href="https://twitch.tv/helpasaurking/about"
+                target="_blank"
+                rel="noopener,noreferrer"
+              >
+                <i className="fa-solid fa-robot"></i> Twitch Bot
+              </Nav.Link>
+              {/* <NavDropdown
+                title={
+                  <>
+                    <i className="fa-solid fa-link"></i> Quick Links
+                  </>
+                }
+                id="quick-links-dropdown"
+              >
+                <NavDropdown.Item
+                  href="https://alttp-wiki.net/index.php/Main_Page"
+                  target="_blank"
+                  rel="noopener,noreferrer"
+                >
+                  Wiki
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="https://spannerisms.github.io/lttphack/"
+                  target="_blank"
+                  rel="noopener,noreferrer"
+                >
+                  Practice Hack
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  href="https://strats.alttp.run/"
+                  target="_blank"
+                  rel="noopener,noreferrer"
+                >
+                  Room LBs
+                </NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="https://discord.gg/8cskCK4">
+                  Discord
+                </NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+            <Nav className="justify-content-end">
+              <Nav.Link
+                href="https://github.com/greenham/helpasaur-king"
+                target="_blank"
+                rel="noopener,noreferrer"
+              >
+                <i className="fa-brands fa-github"></i> GitHub
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
