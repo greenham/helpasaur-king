5cde0123ee58a2f70e379b155d727c6fadde023f
Chris Ham <431647+greenham@users.noreply.github.com>
2023-11-13 22:31:56 -0800

    command searching

--- web/src/components/CommandsList.tsx
+++ web/src/components/CommandsList.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Alert from "react-bootstrap/Alert";
 import Badge from "react-bootstrap/Badge";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 import Stack from "react-bootstrap/Stack";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
@@ -16,24 +19,63 @@ const CommandsList: React.FunctionComponent<CommandsListProps> = () => {
     document.title = "Commands | Helpasaur King";
   }, []);
 
+  const [searchQuery, setSearchQuery] = useState("");
   const {
     data: commands,
     isLoading: commandsLoading,
     isError: commandsError,
   } = useCommands();
 
-  const sortedCommands = sortCommandsAlpha(commands);
+  const filteredCommands =
+    searchQuery.length > 0
+      ? commands.filter(
+          (c) =>
+            c.command.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            c.aliases.some((alias) =>
+              alias.toLowerCase().includes(searchQuery.toLowerCase())
+            ) ||
+            c.response.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : commands;
+  const sortedCommands = sortCommandsAlpha(filteredCommands);
 
   return (
     <Container id="commands" className="mt-5">
       <h1>
         <i className="fa-solid fa-terminal"></i> Commands
       </h1>
-      <p>
+      <p className="lead">
         Each of these commands will work via the Discord or Twitch bots,
         preceded by their configured prefix, which is <code>!</code> by default.
         e.g. <code>!nmg</code>
       </p>
+
+      <InputGroup className="mb-3 mw-50 sticky-top" style={{ top: "56px" }}>
+        <InputGroup.Text id="search-icon">
+          {searchQuery.length > 0 ? (
+            <i
+              className="fa-regular fa-circle-xmark p-2"
+              onClick={() => setSearchQuery("")}
+              style={{ cursor: "pointer" }}
+            ></i>
+          ) : (
+            <i className="fa-solid fa-magnifying-glass p-2"></i>
+          )}
+        </InputGroup.Text>
+        <Form.Control
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          size="lg"
+        />
+      </InputGroup>
+
+      {sortedCommands.length === 0 && (
+        <Alert>
+          No results found for <strong>{searchQuery}</strong>.
+        </Alert>
+      )}
 
       <Stack gap={5} className="d-xl-none">
         {sortedCommands.map((c, idx) => (
@@ -53,31 +95,33 @@ const CommandsList: React.FunctionComponent<CommandsListProps> = () => {
         ))}
       </Stack>
 
-      <Table striped bordered hover responsive className="d-xl-block">
-        <thead>
-          <tr>
-            <th className="text-end mw-25">Command</th>
-            <th>Response</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedCommands.map((c, index) => {
-            return (
-              <tr key={`command-${index}`}>
-                <td className="align-middle text-end">
-                  <code className="fs-3">{c.command}</code>
-                  <CommandAliasesStack aliases={c.aliases} />
-                </td>
-                <td className="align-middle">
-                  <p className="lead">
-                    <LinkifyText text={c.response} />
-                  </p>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      {sortedCommands.length > 0 && (
+        <Table striped bordered hover className="d-none d-xl-block">
+          <thead>
+            <tr>
+              <th className="text-end w-25">Command</th>
+              <th className="w-75">Response</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedCommands.map((c, index) => {
+              return (
+                <tr key={`command-${index}`}>
+                  <td className="align-middle text-end">
+                    <code className="fs-3">{c.command}</code>
+                    <CommandAliasesStack aliases={c.aliases} />
+                  </td>
+                  <td className="align-middle">
+                    <p className="lead">
+                      <LinkifyText text={c.response} />
+                    </p>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
     </Container>
   );
 };
