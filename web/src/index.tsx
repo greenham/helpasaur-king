2ee82416d201f620e54f5aa62990247e67f46a70
Chris Ham <431647+greenham@users.noreply.github.com>
2023-11-15 08:28:00 -0800

    redirect livestreams -> streams

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 
 import App from "./App";
 import ErrorPage from "./error";
@@ -21,6 +25,12 @@ const router = createBrowserRouter([
       {
         path: "streams",
         element: <LivestreamsPage />,
+      },
+      {
+        path: "livestreams",
+        loader: async () => {
+          return redirect("/streams");
+        },
       },
       {
         path: "commands",
