2f334e3b77967ec06e5ad1006a1018c67d3249e9
Chris Ham <431647+greenham@users.noreply.github.com>
2023-11-14 17:39:36 -0800

    add resource links back in

--- web/src/components/TopNav.tsx
+++ web/src/components/TopNav.tsx
@@ -7,6 +7,47 @@ import { LinkContainer } from "react-router-bootstrap";
 
 function TopNav() {
   const logo = new URL("/src/img/logo.png", import.meta.url).toString();
+  const resources = [
+    {
+      href: "https://alttp-wiki.net/index.php/Main_Page",
+      target: "_blank",
+      rel: "noopener,noreferrer",
+      icon: "fa-solid fa-caret-right",
+      text: "Speedrun Wiki",
+    },
+    {
+      href: "https://spannerisms.github.io/lttphack/",
+      target: "_blank",
+      rel: "noopener,noreferrer",
+      icon: "fa-solid fa-caret-right",
+      text: "Practice Hack",
+    },
+    {
+      href: "https://strats.alttp.run/",
+      target: "_blank",
+      rel: "noopener,noreferrer",
+      icon: "fa-solid fa-caret-right",
+      text: "Strat Hub",
+    },
+    {
+      href: "http://www.speedrun.com/alttp",
+      target: "_blank",
+      rel: "noopener,noreferrer",
+      icon: "fa-solid fa-caret-right",
+      text: "Leaderboards",
+    },
+    {
+      divider: true, // Divider element
+    },
+    {
+      href: "https://discord.gg/8cskCK4",
+      target: "_blank",
+      rel: "noopener,noreferrer",
+      icon: "fa-brands fa-discord",
+      text: "Discord",
+    },
+  ];
+
   return (
     <>
       <Navbar expand="lg" bg="primary" sticky="top">
@@ -26,64 +67,54 @@ function TopNav() {
             <Nav className="me-auto">
               <LinkContainer to="commands">
                 <Nav.Link>
-                  <i className="fa-solid fa-terminal"></i> Commands
+                  <i className="fa-solid fa-terminal"></i>&nbsp;&nbsp;Commands
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="streams">
                 <Nav.Link>
-                  <i className="fa-brands fa-twitch"></i> ALttP Streams
+                  <i className="fa-brands fa-twitch"></i>&nbsp;&nbsp;ALttP
+                  Streams
                 </Nav.Link>
               </LinkContainer>
+              <NavDropdown
+                title={
+                  <>
+                    <i className="fa-solid fa-link"></i>&nbsp;&nbsp;Resources
+                  </>
+                }
+                id="resources-dropdown"
+              >
+                {resources.map((resource, index) =>
+                  resource.divider ? (
+                    <NavDropdown.Divider key={index} />
+                  ) : (
+                    <NavDropdown.Item
+                      key={index}
+                      href={resource.href}
+                      target={resource.target}
+                      rel={resource.rel}
+                    >
+                      <i className={resource.icon}></i>&nbsp;&nbsp;
+                      {resource.text}
+                    </NavDropdown.Item>
+                  )
+                )}
+              </NavDropdown>
+            </Nav>
+            <Nav className="justify-content-end">
               <Nav.Link
                 href="https://twitch.tv/helpasaurking"
                 target="_blank"
                 rel="noopener,noreferrer"
               >
-                <i className="fa-solid fa-robot"></i> Twitch Bot
+                <i className="fa-solid fa-robot"></i>&nbsp;&nbsp;Twitch Bot
               </Nav.Link>
-              {/* <NavDropdown
-                title={
-                  <>
-                    <i className="fa-solid fa-link"></i> Quick Links
-                  </>
-                }
-                id="quick-links-dropdown"
-              >
-                <NavDropdown.Item
-                  href="https://alttp-wiki.net/index.php/Main_Page"
-                  target="_blank"
-                  rel="noopener,noreferrer"
-                >
-                  Wiki
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href="https://spannerisms.github.io/lttphack/"
-                  target="_blank"
-                  rel="noopener,noreferrer"
-                >
-                  Practice Hack
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href="https://strats.alttp.run/"
-                  target="_blank"
-                  rel="noopener,noreferrer"
-                >
-                  Room LBs
-                </NavDropdown.Item>
-
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="https://discord.gg/8cskCK4">
-                  Discord
-                </NavDropdown.Item>
-              </NavDropdown> */}
-            </Nav>
-            <Nav className="justify-content-end">
               <Nav.Link
                 href="https://github.com/greenham/helpasaur-king"
                 target="_blank"
                 rel="noopener,noreferrer"
               >
-                <i className="fa-brands fa-github"></i> GitHub
+                <i className="fa-brands fa-github"></i>&nbsp;&nbsp;GitHub
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
