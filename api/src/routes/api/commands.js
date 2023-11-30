c9d11e8363bba885dd02a440c0e095d707ef2e17
Chris Ham <431647+greenham@users.noreply.github.com>
2023-11-29 22:42:16 -0800

    await the promise

--- api/src/routes/api/commands.js
+++ api/src/routes/api/commands.js
@@ -51,7 +51,7 @@ router.delete("/:id", requireAuthKey, async (req, res) => {
 
 router.post("/logs", requireAuthKey, async (req, res) => {
   try {
-    const commandLog = CommandLog.create(req.body);
+    const commandLog = await CommandLog.create(req.body);
     res.status(200).json(commandLog);
   } catch (err) {
     res.status(500).json({ message: err.message });
