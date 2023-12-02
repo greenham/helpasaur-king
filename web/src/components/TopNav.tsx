60f010617caea7986e6746ecfff5b8185a546f16
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-01 23:15:48 -0800

    buttonize login dark bg for profile img

--- web/src/components/TopNav.tsx
+++ web/src/components/TopNav.tsx
@@ -6,6 +6,7 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
+import { Button } from "react-bootstrap";
 import { Image } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { UserContext } from "../contexts/user";
@@ -104,6 +105,8 @@ function TopNav() {
                   Streams
                 </Nav.Link>
               </LinkContainer>
+            </Nav>
+            <Nav>
               <Nav.Link
                 href="https://twitch.tv/helpasaurking"
                 target="_blank"
@@ -151,10 +154,10 @@ function TopNav() {
                       <Image
                         src={user.twitchUserData.profile_image_url}
                         roundedCircle
-                        className="bg-light ml-3"
+                        className="bg-dark ml-3"
                         alt={"Logged in as " + user.twitchUserData.display_name}
-                        width={36}
-                        height={36}
+                        width={32}
+                        height={32}
                       />
                     }
                     id="user-dropdown"
@@ -163,13 +166,16 @@ function TopNav() {
                       href={API_LOGOUT_URL}
                       rel="noopener,noreferrer"
                     >
-                      <i className="fa-solid fa-key"></i>&nbsp;&nbsp;Log Out
+                      <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                      &nbsp;&nbsp;Log Out
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Navbar.Text>
               ) : (
                 <Nav.Link href={TWITCH_LOGIN_URL} rel="noopener,noreferrer">
-                  <i className="fa-solid fa-key"></i>&nbsp;&nbsp;Log In
+                  <Button variant="primary">
+                    <i className="fa-solid fa-key"></i>&nbsp;&nbsp;Log In
+                  </Button>
                 </Nav.Link>
               )}
             </Nav>
