f5dc098427485f5232a8a38e062f260a37eacefa
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-03 21:31:46 -0800

    convert commands to use react-query

--- web/src/utils/apiService.ts
+++ web/src/utils/apiService.ts
@@ -3,3 +3,7 @@ const API_URL = process.env.API_URL;
 export const helpaApiFetcher = (url: string) => {
   return fetch(`${API_URL}${url}`).then((data) => data.json());
 };
+
+export const getCommands = () => {
+  return fetch(`${API_URL}/commands`).then((data) => data.json());
+};
