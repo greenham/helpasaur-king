f6cf068ebd14b4e4a3b179db737938687a40f732
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-06 20:12:41 -0800

    someone fire it up

--- web/src/components/LivestreamsList.tsx
+++ web/src/components/LivestreamsList.tsx
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import StreamCard from "./StreamCard";
 import { filterStreams } from "../utils/utils";
 import { StreamAlertsConfig, TwitchStream } from "../types/streams";
+import { Alert } from "react-bootstrap";
 
 interface LivestreamsListProps {
   streams: TwitchStream[];
@@ -16,6 +17,15 @@ const LivestreamsList: React.FunctionComponent<LivestreamsListProps> = (
   const { streams, config } = props;
   const filteredStreams = filterStreams(streams, config);
   const mergedStreams = filteredStreams.featured.concat(filteredStreams.other);
+
+  if (mergedStreams.length === 0) {
+    return (
+      <Alert>
+        <i className="fa-solid fa-triangle-exclamation px-1"></i> Someone needs
+        to fire it up!
+      </Alert>
+    );
+  }
 
   return (
     <Row xs={1} md={2} lg={3} className="row-gap-5 col-gap-4">
