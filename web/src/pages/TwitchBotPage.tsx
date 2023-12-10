8add55dce6e7989c4af59ab971db16c464424f1f
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-10 08:49:57 -0800

    modal confirmation for leaving

--- web/src/pages/TwitchBotPage.tsx
+++ web/src/pages/TwitchBotPage.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Alert, Button, Container } from "react-bootstrap";
+import { Alert, Button, Container, Modal } from "react-bootstrap";
 import { useEffect } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { UserContext } from "../contexts/user";
@@ -15,13 +15,10 @@ import { getTwitchLoginUrl } from "../utils/utils";
 interface TwitchBotPageProps {}
 
 const TwitchBotPage: React.FunctionComponent<TwitchBotPageProps> = () => {
-  const toast = useToast();
-
   useEffect(() => {
     document.title = "Twitch Bot | Helpasaur King";
   }, []);
 
-  const queryClient = useQueryClient();
   const userContext = React.useContext(UserContext) as UserContextType;
   const { data: user } = userContext;
 
@@ -29,26 +26,6 @@ const TwitchBotPage: React.FunctionComponent<TwitchBotPageProps> = () => {
     queryKey: ["twitchBotConfig"],
     queryFn: getTwitchBotConfig,
   });
-
-  const handleJoinRequest = async () => {
-    const joinResult = await joinTwitchChannel();
-    if (joinResult.result) {
-      toast.success("Successfully joined your channel!");
-      queryClient.invalidateQueries({ queryKey: ["twitchBotConfig"] });
-    } else {
-      toast.warning("Unable to join your channel!");
-    }
-  };
-
-  const handleLeaveRequest = async () => {
-    const leaveResult = await leaveTwitchChannel();
-    if (leaveResult.result) {
-      toast.success("Successfully left your channel!");
-      queryClient.invalidateQueries({ queryKey: ["twitchBotConfig"] });
-    } else {
-      toast.warning("Unable to leave your channel!");
-    }
-  };
 
   return (
     <Container id="twitch-bot-page" className="my-5">
@@ -76,8 +53,6 @@ const TwitchBotPage: React.FunctionComponent<TwitchBotPageProps> = () => {
         <TwitchUserBotManagement
           user={user}
           twitchBotConfig={twitchBotConfig}
-          handleJoinRequest={handleJoinRequest}
-          handleLeaveRequest={handleLeaveRequest}
         />
       )}
     </Container>
@@ -87,14 +62,35 @@ const TwitchBotPage: React.FunctionComponent<TwitchBotPageProps> = () => {
 interface TwitchUserBotManagementProps {
   user: IUser;
   twitchBotConfig: { botHasJoined: boolean };
-  handleJoinRequest: () => void;
-  handleLeaveRequest: () => void;
 }
 const TwitchUserBotManagement: React.FunctionComponent<
   TwitchUserBotManagementProps
 > = (props) => {
-  const { user, twitchBotConfig, handleJoinRequest, handleLeaveRequest } =
-    props;
+  const { user, twitchBotConfig } = props;
+
+  const queryClient = useQueryClient();
+  const toast = useToast();
+
+  const handleJoinRequest = async () => {
+    const joinResult = await joinTwitchChannel();
+    if (joinResult.result) {
+      toast.success("Successfully joined your channel!");
+      queryClient.invalidateQueries({ queryKey: ["twitchBotConfig"] });
+    } else {
+      toast.warning("Unable to join your channel!");
+    }
+  };
+
+  const handleLeaveRequest = async () => {
+    const leaveResult = await leaveTwitchChannel();
+    if (leaveResult.result) {
+      toast.success("Successfully left your channel!");
+      queryClient.invalidateQueries({ queryKey: ["twitchBotConfig"] });
+    } else {
+      toast.warning("Unable to leave your channel!");
+    }
+    handleCloseLeaveModal();
+  };
 
   if (!twitchBotConfig?.botHasJoined) {
     return (
@@ -113,18 +109,55 @@ const TwitchUserBotManagement: React.FunctionComponent<
     );
   }
 
+  const [showLeaveModal, setShowLeaveModal] = React.useState(false);
+  const handleShowLeaveModal = () => setShowLeaveModal(true);
+  const handleCloseLeaveModal = () => setShowLeaveModal(false);
+
   return (
-    <Alert variant="dark" className="p-5">
-      <h2>Thanks for using HelpasaurKing!</h2>
-      <p className="lead my-5">
-        Eventually there will be more settings here like a custom command
-        prefix, enable/disable commands, maybe even some Twitch integrations
-        with channel points, polls, etc.
-      </p>
-      <Button variant="danger" onClick={handleLeaveRequest} size="lg">
-        <i className="fa-solid fa-right-from-bracket px-1"></i> Leave my channel
-      </Button>
-    </Alert>
+    <>
+      <Alert variant="dark" className="p-5">
+        <h2>Thanks for using HelpasaurKing!</h2>
+        <p className="lead my-5">
+          Eventually there will be more settings here like a custom command
+          prefix, enable/disable commands, maybe even some Twitch integrations
+          with channel points, polls, etc.
+        </p>
+        <Button variant="danger" onClick={handleShowLeaveModal} size="lg">
+          <i className="fa-solid fa-right-from-bracket px-1"></i> Leave my
+          channel
+        </Button>
+      </Alert>
+      <ConfirmLeaveModal
+        show={showLeaveModal}
+        handleClose={handleCloseLeaveModal}
+        handleConfirm={handleLeaveRequest}
+      />
+    </>
+  );
+};
+
+const ConfirmLeaveModal: React.FunctionComponent<{
+  show: boolean;
+  handleClose: () => void;
+  handleConfirm: () => void;
+}> = ({ show, handleClose, handleConfirm }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want the bot to leave your channel?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="dark" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleConfirm}>
+          Yes, leave my channel
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
