929c196ba7f23efad2b985e7d295f942bbcf7d36
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-09 19:39:35 -0800

    catch all query errors

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import TopNav from "./components/TopNav";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryCache,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import UserProvider from "./components/UserProvider";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.state.data !== undefined) {
+        console.error(`Something went wrong: ${error.message}`);
+      }
+    },
+  }),
+});
 
 interface AppProps {}
 const App: React.FunctionComponent<AppProps> = () => {
