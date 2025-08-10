02710be4955fd10c77fc2a1f38f1b2d8fd3b1a25
Chris Ham <431647+greenham@users.noreply.github.com>
2025-08-10 13:03:32 -0700

    formatting

--- api/src/constants.js
+++ api/src/constants.js
@@ -1,8 +1,20 @@
 // API Constants - Source of truth for configuration options
 
 // Allowed command prefixes for Twitch/Discord bots
-// Note: Forward slash (/) excluded for Discord compatibility
-const ALLOWED_COMMAND_PREFIXES = ["!", "@", "#", "$", "%", "&", "*", "+", "-", "=", "?", "~"];
+const ALLOWED_COMMAND_PREFIXES = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "&",
+  "*",
+  "+",
+  "-",
+  "=",
+  "?",
+  "~",
+];
 
 module.exports = {
   ALLOWED_COMMAND_PREFIXES,
