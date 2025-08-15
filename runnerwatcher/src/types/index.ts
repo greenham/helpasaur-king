7b774f0cb9e32a772da6265cdc155117694b208f
Chris Ham <431647+greenham@users.noreply.github.com>
2025-08-14 21:58:54 -0700

    remove unused types

--- runnerwatcher/src/types/index.ts
+++ runnerwatcher/src/types/index.ts
@@ -21,29 +21,3 @@ export interface StreamAlertPayload {
   startedAt?: string
   type?: "live" | "offline"
 }
-
-// Type definition for node-twitch StreamData
-export interface StreamData {
-  id: string
-  user_id: string
-  user_login: string
-  user_name: string
-  game_id: string
-  game_name: string
-  type: string
-  title: string
-  viewer_count: number
-  started_at: string
-  language: string
-  thumbnail_url: string
-  tag_ids: string[]
-  is_mature: boolean
-}
-
-export interface CachedStream {
-  id: string // stream id
-  user_id: string
-  title: string
-  game_id: string
-  lastAlertedAt: number // timestamp
-}
